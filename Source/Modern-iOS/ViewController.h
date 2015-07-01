09fb5b2ea4599e5c8cbcb2b1a2e27d80ca88f1f7
Elmar Tampe <elmo@mountaincoders.ch>
2015-07-01 13:43:59 +0200

    Project setup.

---
+++ Source/Modern-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Modern-iOS
+//
+//  Created by Elmar Tampe on 01/07/15.
+//  Copyright © 2015 Elmar Tampe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Source/Modern-iOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Modern-iOS
+//
+//  Created by Elmar Tampe on 01/07/15.
+//  Copyright © 2015 Elmar Tampe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
